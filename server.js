a1da77154a329c73166079fc193b00f1a6611d90
David Jaramillo <jaramillo.david@outlook.com>
2020-09-23 15:22:36 -0500

    added server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const {sequelize, DBconnection} = require('./config/db');
+
+const usersRoute = require("./routes/users");
+app.use("/",usersRoute);
+
+const port = process.env.PORT || 9000;
+app.listen(port, () => { 
+  console.log('Server listening on port 9000');
+  DBconnection();
+});
